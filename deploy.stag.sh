9b0e6ee184fa25adfdf0498ea86ac7763d1d3a0a
maplethorpej <jeradmaplethorpe@gmail.com>
2021-03-29 16:39:11 -0500

    use port with database_url; k8s staging configuration

--- deploy.stag.sh
+++ deploy.stag.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 doctl registry login
-docker build -f stag.Dockerfile -t packstack .
+docker build -t packstack .
 docker tag packstack:latest registry.digitalocean.com/packstack/packstack-api
 docker push registry.digitalocean.com/packstack/packstack-api
