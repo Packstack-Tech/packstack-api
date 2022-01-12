679722c12393d30324fa35986f37e1e01117334d
maplethorpej <jeradmaplethorpe@gmail.com>
2022-01-11 20:15:12 -0600

    update allowed origins

--- app/main.py
+++ app/main.py
@@ -12,7 +12,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         'http://localhost:3000',
-        'http://127.0.0.1:3000'
+        'http://127.0.0.1:3000',
+        'https://packstack.io',
+        'https://beta.packstack.io'
     ],
     allow_credentials=True,
     allow_methods=["*"],
