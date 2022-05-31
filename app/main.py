e1df05d730639b9b02a41d594cc3790f2f3d2ab2
maplethorpej <jeradmaplethorpe@gmail.com>
2022-05-31 17:39:23 -0500

    update cors

--- app/main.py
+++ app/main.py
@@ -14,7 +14,8 @@ app.add_middleware(
         'http://localhost:3000',
         'http://127.0.0.1:3000',
         'https://packstack.io',
-        'https://beta.packstack.io'
+        'https://staging.packstack.io',
+        'https://packstack-social.vercel.app'
     ],
     allow_credentials=True,
     allow_methods=["*"],
