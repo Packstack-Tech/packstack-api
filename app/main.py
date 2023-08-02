288487f21f9a4d50ffcfe98c0dd0ff4d2db5307e
Jerad Maplethorpe <jeradmaplethorpe@gmail.com>
2023-08-02 11:01:55 -0500

    origins

--- app/main.py
+++ app/main.py
@@ -20,6 +20,8 @@ app.add_middleware(
     allow_origins=[
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
     ],
     allow_credentials=True,
     allow_methods=["*"],
