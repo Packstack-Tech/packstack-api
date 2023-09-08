abcd894aa438189963ca81533a608027b0cef26f
Jerad Maplethorpe <jeradmaplethorpe@gmail.com>
2023-09-08 14:18:12 -0500

    CORS

--- app/main.py
+++ app/main.py
@@ -22,6 +22,8 @@ app.add_middleware(
         'http://127.0.0.1:3000',
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'https://packstack.io',
+        'https://*.packstack.io',
     ],
     allow_credentials=True,
     allow_methods=["*"],
