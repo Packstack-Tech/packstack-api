3b7c553ed6d9e089609fb6cccf626b2bb534c081
Jerad Maplethorpe <jeradmaplethorpe@gmail.com>
2026-05-12 11:25:09 -0500

    rm paywalled features

--- app/api/trip.py
+++ app/api/trip.py
@@ -130,7 +130,7 @@ def create(payload: TripType, user: User = Depends(authenticate)):
     except Exception:
         raise HTTPException(400, "Unable to create trip.")
 
-    if user.is_subscribed and payload.location and payload.location.strip():
+    if payload.location and payload.location.strip():
         new_trip.enrich_status = "pending"
         db.session.commit()
         db.session.refresh(new_trip)
@@ -165,7 +165,7 @@ def update(payload: TripUpdate, user: User = Depends(authenticate)):
             (payload.start_date and payload.start_date != old_start_date) or
             (payload.end_date and payload.end_date != old_end_date)
         )
-        if user.is_subscribed and trip.location and (location_changed or dates_changed):
+        if trip.location and (location_changed or dates_changed):
             trip.enrich_status = "pending"
 
         db.session.commit()
